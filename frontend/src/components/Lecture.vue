85f9c1c12bedc88896f55e0d0e3dfa05209cc18a
JunhoeKim <joon.hoe94@gmail.com>
2018-03-27 21:55:50 +0900

    on going

---
+++ frontend/src/components/Lecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>
+      <div class=".col-md-2 .col-md-offset-1"><img src="../assets/ollit_logo_korean.png" /></div>
+    </header>
+    <section>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+header {
+  height: 200px;
+  background: url('../assets/main_background_image.png');
+  background-blend-mode: multiply;
+  background-color: rgba(0, 0, 0, 0.5);
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+
+</style>
+
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Lecture from '@/components/Lecture'
 
 Vue.use(Router)
 
@@ -9,6 +10,10 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
+      component: Lecture
+    },
+    {
+      path: '/lecture',
       component: HelloWorld
     }
   ]
