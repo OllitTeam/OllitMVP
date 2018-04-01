4196dca069efb7c97b56dc8c0a3d6c66a3c13d49
JunhoeKim <joon.hoe94@gmail.com>
2018-04-01 18:33:01 +0900

    update lecture page before data binding, only CSS

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
 import Lecture from '@/components/Lecture'
 
 Vue.use(Router)
@@ -9,12 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: Lecture
+      name: 'HelloWOrld',
+      component: Main
     },
     {
       path: '/lecture',
-      component: HelloWorld
+      component: Lecture
     }
   ]
 })
